f28cb88167633459171478cb563ee290cb449e02
Rajibul Hasan <rajibulnsu@yahoo.com>
2020-05-09 22:13:59 +0200

    Updates

---
+++ precache-manifest.ef277360c0dbd917e509327e6271ec12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddec38894c50256ed6f09fd883566b09",
+    "url": "/scalable/index.html"
+  },
+  {
+    "revision": "75fd3f9e02aacebee833",
+    "url": "/scalable/static/js/2.45288a84.chunk.js"
+  },
+  {
+    "revision": "78b86c0095c74f94992db9795d80e258",
+    "url": "/scalable/static/js/2.45288a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc43a116a9aeec3dcc1",
+    "url": "/scalable/static/js/main.7fa7ffb8.chunk.js"
+  },
+  {
+    "revision": "d1b373bcbf4eca2a3767",
+    "url": "/scalable/static/js/runtime-main.8b2d2968.js"
+  }
+]);
